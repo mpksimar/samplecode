277b3e7b3acd4399d650ca2b6374ce22f037b92d
mpksimar <mpksimar@gmail.com>
2021-04-26 17:58:56 -0400

    committed sample code

---
+++ src/components/App.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import './App.css';
+import ListItems from './list/ListItems';
+import Data from '../data/Data.json';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faTrash);
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state= {
+      items: [],
+      currentItem:{
+        text: '',
+        key: ''
+      }
+    }
+    this.handleInput= this.handleInput.bind(this);
+    this.addItem= this.addItem.bind(this);
+    this.deleteItem= this.deleteItem.bind(this);
+    this.setUpdate= this.setUpdate.bind(this);
+  }
+
+  componentDidMount(){
+    this.setState({
+      items: Data
+    })
+  }
+
+  handleInput(e){
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+
+  addItem(e){
+    e.preventDefault();//this will prevent from the elements default behavior hover operations.
+    const newItem= this.state.currentItem;
+    console.log(newItem);
+    if(newItem.text!== ""){
+      const newItems= [...this.state.items, newItem];
+      this.setState({
+        items: newItems,
+        currentItem: {
+          text: '',
+          key: ''
+        }
+      })
+    }
+  }
+
+  deleteItem(key){
+    const filteredItems= this.state.items.filter(item =>
+      item.key!==key);
+    this.setState({
+      items: filteredItems
+    })
+  }
+
+  setUpdate(text, key){
+    const items= this.state.items;
+    items.map(item=>{
+      if(item.key === key){
+        item.text= text;
+      }
+    })
+    this.setState({
+      items: items
+    })
+
+  }
+  
+  render(){
+    return(
+     <div className= "App">
+       <header>
+       <form id= "to-do-form" onSubmit= {this.addItem}>
+       <input type="text" placeholder="Enter text here!" value= {this.state.currentItem.text} onChange={this.handleInput}></input>
+         <button type= "submit">Add</button>
+       </form>
+     </header>
+     <ListItems items= {this.state.items} deleteItem= {this.deleteItem} setUpdate={this.setUpdate}/>
+    
+   
+    
+     </div>
+    )
+  }
+}
+export default App;
